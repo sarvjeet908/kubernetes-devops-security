70219ce82d76cfda1a70007176ca7a929cb10c4b
sarvjeet Kumar Singh <121434029+sarvjeet908@users.noreply.github.com>
2026-08-11 22:01:26 +0530

    Update integration-test.sh

--- integration-test.sh
+++ integration-test.sh
@@ -1,40 +1,19 @@
-################################## integration-test.sh ################################## 
-#!/bin/bash
+response=$(curl -s "$applicationURL:$PORT$applicationURI" | tr -d '\r\n')
+http_code=$(curl -s -o /dev/null -w "%{http_code}" "$applicationURL:$PORT$applicationURI")
 
-#integration-test.sh
+echo "Response: [$response]"
+echo "HTTP Code: [$http_code]"
 
-sleep 10s
-
-PORT=$(kubectl -n default get svc ${serviceName} -o json | jq .spec.ports[].nodePort)
-
-echo $PORT
-echo $applicationURL:$PORT/$applicationURI
-
-if [[ ! -z "$PORT" ]];
-then
-
-    response=$(curl -s $applicationURL:$PORT$applicationURI)
-    http_code=$(curl -s -o /dev/null -w "%{http_code}" $applicationURL:$PORT$applicationURI)
-
-    if [[ "$response" == "100" ]];
-        then
-            echo "Increment Test Passed"
-        else
-            echo "Increment Test Failed"
-            exit 1;
-    fi;
-
-    if [[ "$http_code" == "200" ]];
-        then
-            echo "HTTP Status Code Test Passed"
-        else
-            echo "HTTP Status code is not 200"
-            exit 1;
-    fi;
-
+if [[ "$response" == "100" ]]; then
+    echo "Increment Test Passed"
 else
-        echo "The Service does not have a NodePort"
-        exit 1;
-fi;
+    echo "Increment Test Failed"
+    exit 1
+fi
 
-################################## integration-test.sh ################################## 
+if [[ "$http_code" == "200" ]]; then
+    echo "HTTP Status Code Test Passed"
+else
+    echo "HTTP Status code is not 200"
+    exit 1
+fi
